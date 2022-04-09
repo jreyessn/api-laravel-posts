c4f8db133ecd05f6e6afc1c44360856803ae9c2d
Juan Reyes <snjuank@gmail.com>
2022-04-09 06:50:19 -0400

    feature(users): Enpoints (list, show, store) para usuarios

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UsersController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::with(["videos", "posts"])->paginate();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name"      => "required|string|max:255",
+            "email"     => "required|email|unique:users,email",
+            "password"  => "string|min:4"
+        ]);
+
+        DB::beginTransaction();
+
+        try{
+            $user = User::create($request->all());
+            
+            DB::commit();
+
+            return response()->json([
+                "message" => "Registro éxitoso",
+                "data"    => $user
+            ], 201);
+
+        }catch(\Exception $e){
+            DB::rollback();
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return [
+            "data" => $user->load(["videos", "posts"])
+        ];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
