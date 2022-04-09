3b17c2368eb32795cf01a97e539bb53e26f43ef7
Juan Reyes <snjuank@gmail.com>
2022-04-09 08:13:53 -0400

    feature(auth): Autenticación de usuario y middleware de rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\VideosController;
@@ -17,8 +18,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResources([
-    "users"  => UsersController::class,
-    "posts"  => PostsController::class,
-    "videos" => VideosController::class
-]);
+Route::group(['middleware' => 'api'], function () {
+
+    Route::group(["prefix" => "auth"], function(){
+        Route::post('sign-in',  [AuthController::class, 'login']);
+        Route::post('sign-out', [AuthController::class, 'logout']);
+        Route::post('refresh',  [AuthController::class, 'refresh']);
+        Route::get( 'me',       [AuthController::class, 'me']);
+    });
+
+    Route::group(["middleware" => "auth"], function(){
+        Route::apiResources([
+            "users"  => UsersController::class,
+            "posts"  => PostsController::class,
+            "videos" => VideosController::class
+        ]);
+    });
+});
